e671d44b15fa188edf231be5680b1a09d7fc3cca
Priyanshu Gupta <priyanshu711403@gmail.com>
2023-12-23 02:01:57 +0530

    Utils created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // require("dotenv").config();// can not be used here as this type has been set to module, only import will work
 import dotenv from "dotenv";
-
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => console.log("ERROR:" + error));
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running on port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB connection failed", err);
+  });
+
 /*import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 
