b690cbbc8bef4b9bb326108e69db95089f528a57
Priyanshu Gupta <priyanshu711403@gmail.com>
2023-12-24 11:44:54 +0530

    user model mistake fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-userSchema.methods.generateAccessToken = async function (password) {
+userSchema.methods.generateAccessToken = async function () {
   return jwt.sign(
     {
       _id: this._id,
@@ -73,7 +73,7 @@ userSchema.methods.generateAccessToken = async function (password) {
   );
 };
 
-userSchema.methods.generateRefreshToken = async function (password) {
+userSchema.methods.generateRefreshToken = async function () {
   return jwt.sign(
     {
       _id: this._id,
