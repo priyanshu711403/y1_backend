703f2b25e2ed266de9c462c218ead4249c076912
Priyanshu Gupta <priyanshu711403@gmail.com>
2024-01-15 12:40:32 +0530

    add video routes

--- src/app.js
+++ src/app.js
@@ -18,8 +18,9 @@ app.use(cookieParser());
 
 // routes import
 import userRouter from "./routes/user.routers.js";
-
+import videoRouter from "./routes/video.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
 
 export { app };
