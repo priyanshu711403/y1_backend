135cf0feec3df20752aeb1292e0d19730a02db28
Priyanshu Gupta <priyanshu711403@gmail.com>
2024-11-08 22:59:40 +0530

    add getAllvideos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,6 +1,5 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose, { Types, isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
-// import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -11,7 +10,65 @@ import {
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+  page = parseInt(page);
+  const sortTypeValue = parseInt(sortType);
+  limit = parseInt(limit);
+
   //TODO: get all videos based on query, sort, pagination
+  const skip = (page - 1) * limit;
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: userId,
+        isPublished: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+    {
+      $sort: {
+        [sortBy]: sortTypeValue,
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: limitValue,
+    },
+  ]);
+  if (!videos) throw new ApiError(500, "videos not fetched");
+  Video.aggregatePaginate(videos, { page, limitValue })
+    .then(function (result) {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, { result }, "Fetched videos successfully"));
+    })
+    .catch(function (error) {
+      throw error;
+    });
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
@@ -50,7 +107,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
     duration: videoDuration,
     owner: req.user?._id,
   });
-  if (!video) throw new ApiError(400, "video published unsuccessful");
+  if (!video) throw new ApiError(400, "video not published");
   return res
     .status(201)
     .json(new ApiResponse(201, video, "video published successfully"));
@@ -64,9 +121,42 @@ const getVideoById = asyncHandler(async (req, res) => {
   //check isPublished status and owner
   //return
   if (!videoId) throw new ApiError(400, "videoId is mandatory");
-  const video = await Video.findById(videoId);
-  if (!video) throw new ApiError(404, "No video with given videoId");
-  if (!video.isPublished && !video.owner.equals(req.user._id))
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+  ]);
+
+  if (!video[0]) throw new ApiError(404, "No video with given videoId");
+  // console.log(video.owner);
+
+  if (!video[0].isPublished && !video[0].owner._id.equals(req.user._id))
     // when video in not removed from published and user is not the owner
     throw new ApiError(400, "unauthorized access");
   return res
@@ -78,16 +168,19 @@ const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: update video details like title, description, thumbnail
   if (!videoId) throw new ApiError(400, "videoId is mandatory");
+
   const video = await Video.findById(videoId);
   if (!video.owner.equals(req.user._id))
     throw new ApiError(400, " unauthorized request:video not owned by user");
 
   const { title = video.title, description = video.description } = req.body;
   const thumbnailLocalPath = req.file?.path;
+  //only if a new thumbnail is given
   if (thumbnailLocalPath) {
     const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
     if (!thumbnail)
       throw new ApiError(400, "thumbnail was not uploaded on cloudinary");
+    //delete old thumbnail
     const oldThumbnail = video.thumbnail;
     const isOldThumbnailDeleted = await deleteFromCloudinary(oldThumbnail);
     if (!isOldThumbnailDeleted)
@@ -111,14 +204,17 @@ const deleteVideo = asyncHandler(async (req, res) => {
   //TODO: delete video
   if (!videoId) throw new ApiError(400, "videoId is mandatory");
   const video = await Video.findById(videoId);
+
   if (!video.owner.equals(req.user._id))
     throw new ApiError(400, " unauthorized request:video not owned by user");
+
   const { videoFile, thumbnail } = video;
   const deleteThumbnail = await deleteFromCloudinary(thumbnail);
   const deleteVideo = await deleteFromCloudinary(videoFile);
   if (!deleteThumbnail || !deleteVideo)
     throw new ApiError(400, "video and thumbnail not deleted from cloudinary");
   const deleteResponse = await video.deleteOne({ videoId });
+
   return res
     .status(200)
     .json(
@@ -133,10 +229,13 @@ const deleteVideo = asyncHandler(async (req, res) => {
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   if (!videoId) throw new ApiError(400, "videoId is mandatory");
+
   const video = await Video.findById(videoId);
   if (!video.owner.equals(req.user._id))
     throw new ApiError(400, " unauthorized request:video not owned by user");
+
   video.isPublished = !video.isPublished;
+
   const updatedVideo = await video.save(
     { validateBeforeSave: false },
     {
